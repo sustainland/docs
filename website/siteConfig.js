4e8dbf94024ec239f9aaa891d2e26fca847f1c85
Aymen <labidi@aymen.co>
2020-03-22 19:50:33 +0100

    update sirecofngi

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,8 +30,8 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Sustain',
-  organizationName: 'Vaulab',
+  projectName: 'sustain-docs',
+  organizationName: 'labidiaymen',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
